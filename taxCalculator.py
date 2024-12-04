0372d39a7a2c188831453ff5069b3526b74db642
ant_robot <yaroslav.antonchik@gmail.com>
2024-12-05 00:07:06 +0100

    Add transaction type and split handling

--- taxCalculator.py
+++ taxCalculator.py
@@ -20,18 +20,26 @@ def process_stocks(file_path, taxPeriodEnd):
 def singleStockPrice(transaction):
     return abs(transaction["Spent"] / transaction["Amount"])
 
+def calculateStats(q):
+    spent = 0
+    amount = 0
+    for record in q:
+        spent += record["Spent"]
+        amount += record["Amount"]
+    return (amount, (spent/amount) if amount > 0  else 0)
+    # if len(q):
+    #     print(f'Ticker {q[0]["Ticker"]} amount {amount} average stock price {(spent/amount):.2f}')
+
 def calculate_tax_single_stock(stockData):
-    # print(stockData)
     totalTaxBase = 0
     q = deque()
     for i in range(len(stockData)):
         transaction = stockData.iloc[i].copy()
-        if not math.isclose(transaction["Amount"], 0, rel_tol=1e-9, abs_tol=0.0) and (transaction["Amount"] > 0):
+        if transaction["Transaction"] == "BUY":
             q.append(transaction)
-        else:
+        elif transaction["Transaction"] == "SELL":
             stocks2Sell = abs(transaction["Amount"])
             taxBase = 0
-            # breakpoint()
             while(stocks2Sell > 0):
                 if q[0]["Amount"] > stocks2Sell:
                     taxBase += stocks2Sell*singleStockPrice(transaction) - stocks2Sell*singleStockPrice(q[0])
@@ -45,25 +53,30 @@ def calculate_tax_single_stock(stockData):
             if transaction['Date'] >= pd.to_datetime(taxPeriodStart):
                 # print(f"ticker {stockData.iloc[0]['Ticker']} taxBase {taxBase}")
                 totalTaxBase += taxBase
-    return totalTaxBase
+        elif transaction["Transaction"] == "SPLIT":
+            # print(f'Split {stockData.iloc[0]["Ticker"]} coefficient {transaction["Amount"]}')
+            for record in q:
+                record["Amount"] *= transaction["Amount"]
+    # printBalance(q)    
+    return calculateStats(q) + (totalTaxBase,)
         
 
 # Example usage
-file_path = 'stocks.xlsx'
-taxYear = '2022'
+file_path = 'stocks (2).xlsx'
+taxYear = 2024
 taxPeriodStart = f"{taxYear}-01-01"
 taxPeriodEnd = f"{taxYear}-12-31"
 
-sorted_and_filtered_data = process_stocks(file_path, taxPeriodEnd)
-
 if __name__ == "__main__":
     totalTaxBase = 0
+    sorted_and_filtered_data = process_stocks(file_path, taxPeriodEnd)
     for ticker in sorted_and_filtered_data['Ticker'].unique():
-        if ticker not in ["NVDA", "SPCE"]:
-        # if ticker == "NVDA":
-            taxBase = calculate_tax_single_stock(sorted_and_filtered_data[sorted_and_filtered_data['Ticker'] == ticker])
-            if taxBase != 0:
-                print(f"Ticker {ticker} tax base {taxBase}")
-                totalTaxBase += taxBase
+        amount, avgPrice, taxBase = calculate_tax_single_stock(sorted_and_filtered_data[sorted_and_filtered_data['Ticker'] == ticker])
+        if taxBase != 0:
+            print(f"Ticker {ticker}, taxBase {taxBase}")
+        # print(sorted_and_filtered_data[sorted_and_filtered_data['Ticker'] == ticker])
+        # print(f"Ticker {ticker} left {amount} avgPrice {avgPrice:.2f} earned {taxBase}")
+
+        totalTaxBase += taxBase
     print(f"Total tax base in {taxYear} is {totalTaxBase}")
 
